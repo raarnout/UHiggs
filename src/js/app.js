9c798e2a59bc509d593dbdee3df2b3098e920133
Ronald <ronaldaarnoutse@gmail.com>
2021-12-15 21:17:04 +0100

    cleanup unused code

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,3 @@
 import "../scss/app.scss";
 
 import "./modules/bootstrap";
-// import "./modules/sidebar";
-// import "./modules/theme";
-// import "./modules/feather";
-
-// // Charts
-// import "./modules/chartjs";
-
-// // // Forms
-// import "./modules/flatpickr";
-
-// // Maps
-// import "./modules/vector-maps";
--- src/js/modules/chartjs.js
+++
@@ -1,5 +0,0 @@
-// Usage: https://www.chartjs.org/
-import Chart from "chart.js/auto";
-
-Chart.defaults.global.defaultFontFamily = "'Inter', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
-window.Chart = Chart;
